43ebf3880b4d346373927864cb1436eadaadeeba
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-13 17:23:24 -0700

    Add Flower model, make it possible to create new Flowers

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,4 +1,21 @@
 class FlowersController < ApplicationController
+  def show
+    @flower = Flower.find(params[:id])
+  end
+  
   def new
+  end
+
+  def create
+    @flower = Flower.new(flower_params)
+
+    @flower.save
+    redirect_to @flower
+  end
+
+  private
+
+  def flower_params
+    params.require(:flower).permit(:name, :description)
   end
 end
