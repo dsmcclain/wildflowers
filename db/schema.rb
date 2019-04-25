8eee3cbe07aaf3a5e47fbf2579cf9d6764a35d27
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-25 10:51:49 -0700

    create User model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_182735) do
+ActiveRecord::Schema.define(version: 2019_04_25_172419) do
 
   create_table "flowers", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2019_04_14_182735) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["flower_id"], name: "index_sightings_on_flower_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
