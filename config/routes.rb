61f1e54a4e58c36005e8b236c1fccb8762ee93ba
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-25 10:15:15 -0700

    create users MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get '/signup', to: 'users#new'
   get 'welcome/index'
 
   resources :flowers do
     resources :sightings
   end
-  
-  root 'welcome#index'
 end
