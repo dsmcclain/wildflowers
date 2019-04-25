ecdc2c3c8eb3c577e3927f37c21fb17f46f9abbf
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-25 12:21:23 -0700

    add validation for passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
   private
 
