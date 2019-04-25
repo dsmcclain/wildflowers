81f9e478f460d35fac966e7d6c182dc3da776f52
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-25 12:16:29 -0700

    add password digest and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, 
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 
   private
 
