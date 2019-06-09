87b3eb3f1ef74f6b822f16f3792277c3ad566255
Dylan McClain <dylan.mcclain@gmail.com>
2019-06-09 12:38:36 -0700

    db migration for persistent user sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_171632) do
+ActiveRecord::Schema.define(version: 2019_06_09_193656) do
 
   create_table "flowers", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_171632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
